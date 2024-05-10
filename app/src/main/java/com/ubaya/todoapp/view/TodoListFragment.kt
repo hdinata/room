54e2db40e70623334f30222878ec08c8dcca44fd
Hendra <hdinata@gmail.com>
2024-05-10 15:38:42 +0700

    commit

---
+++ app/src/main/java/com/ubaya/todoapp/view/TodoListFragment.kt
@@ -0,0 +1,83 @@
+package com.ubaya.todoapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ubaya.todoapp.R
+import com.ubaya.todoapp.databinding.FragmentTodoListBinding
+import com.ubaya.todoapp.viewmodel.ListTodoViewModel
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TodoListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TodoListFragment : Fragment() {
+    private lateinit var viewModel: ListTodoViewModel
+    private val todoListAdapter  = TodoListAdapter(arrayListOf())
+
+    private lateinit var binding: FragmentTodoListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentTodoListBinding.inflate(inflater,container,false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnFab.setOnClickListener {
+            val action = TodoListFragmentDirections.actionCreateTodo()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        /*viewModel = ViewModelProvider(this).get(ListTodoViewModel::class.java)
+        viewModel.refresh()
+        binding.recViewTodo.layoutManager = LinearLayoutManager(context)
+        binding.recViewTodo.adapter = todoListAdapter
+
+        */
+
+        //observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.todoLD.observe(viewLifecycleOwner, Observer {
+            todoListAdapter.updateTodoList(it)
+            if(it.isEmpty()) {
+                binding.recViewTodo?.visibility = View.GONE
+                binding.txtError.setText("Your todo still empty.")
+            } else {
+                binding.recViewTodo?.visibility = View.VISIBLE
+            }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == false) {
+                binding.progressLoad?.visibility = View.GONE
+            } else {
+                binding.progressLoad?.visibility = View.VISIBLE
+            }
+        })
+
+        viewModel.todoLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == false) {
+                binding.txtError?.visibility = View.GONE
+            } else {
+                binding.txtError?.visibility = View.VISIBLE
+            }
+        })
+
+    }
+
+}
