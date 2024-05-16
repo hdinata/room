afdd67e7f33719594c9d664d31c968225c553c94
Hendra <hdinata@gmail.com>
2024-05-16 15:21:26 +0700

    commit

--- app/src/main/java/com/ubaya/todoapp/model/TodoDao.kt
+++ app/src/main/java/com/ubaya/todoapp/model/TodoDao.kt
@@ -11,7 +11,7 @@ interface TodoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg todo:Todo)
 
-    @Query("SELECT * FROM todo")
+    @Query("SELECT * FROM todo ORDER BY priority DESC")
     fun selectAllTodo(): List<Todo>
 
     @Query("SELECT * FROM todo WHERE uuid= :id")
@@ -19,4 +19,8 @@ interface TodoDao {
 
     @Delete
     fun deleteTodo(todo:Todo)
+
+    @Query("UPDATE todo SET title=:title, notes=:notes, priority=:priority WHERE uuid = :id")
+    fun update(title:String, notes:String, priority:Int, id:Int)
+
 }
--- app/src/main/java/com/ubaya/todoapp/viewmodel/DetailTodoViewModel.kt
+++ app/src/main/java/com/ubaya/todoapp/viewmodel/DetailTodoViewModel.kt
@@ -2,8 +2,10 @@ package com.ubaya.todoapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.ubaya.todoapp.model.Todo
 import com.ubaya.todoapp.model.TodoDatabase
+import com.ubaya.todoapp.util.buildDb
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -13,16 +15,33 @@ import kotlin.coroutines.CoroutineContext
 class DetailTodoViewModel(application: Application)
     :AndroidViewModel(application), CoroutineScope{
     private val job = Job()
+    val todoLD = MutableLiveData<Todo>()
 
     override val coroutineContext: CoroutineContext
         get() = job + Dispatchers.IO
 
     fun addTodo(list:List<Todo>) {
         launch {
-            val db = TodoDatabase.buildDatabase(
+            /*val db = TodoDatabase.buildDatabase(
                 getApplication()
-            )
+            )*/
+            val db = buildDb(getApplication())
             db.todoDao().insertAll(*list.toTypedArray())
         }
     }
+
+    fun fetch(uuid:Int) {
+        launch {
+            val db = buildDb(getApplication())
+            todoLD.postValue(db.todoDao().selectTodo(uuid))
+        }
+    }
+
+    fun update(title:String, notes:String, priority:Int, uuid:Int) {
+        launch {
+            val db = buildDb(getApplication())
+            db.todoDao().update(title, notes, priority, uuid)
+        }
+    }
+
 }
